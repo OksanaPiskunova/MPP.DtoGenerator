edf273f0708d6679e51bf328e416202d1f454fa2
OksanaPiskunova <piskunova.oksana2014@yandex.ru>
2016-10-17 01:46:42 +0300

    Added the ability to load plug-ins

---
+++ DtoGenerator/DtoGenerator/DtoGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using DtoGenerator.Descriptions;
+using DtoGenerator.Generator;
+
+namespace DtoGenerator
+{
+    public sealed class DtoGenerator
+    {
+        private TypeTable _typeTable;
+
+        public DtoGenerator(int maxThreadCount, string pluginsDirectory)
+        {
+            _typeTable = LoadPlugins(pluginsDirectory);
+        }
+
+        public List<GeneratedCodeItem> GenerateDtoClasses(DtoClassDescription[] dtoClassDescriptions, string classesNamespace)
+        {
+            var codeGenerator = new CodeGenerator(_typeTable);
+            var generatedCodeList = new List<GeneratedCodeItem>();
+
+            foreach (var classDescription in dtoClassDescriptions)
+            {
+                var code = codeGenerator.GenerateCode(classDescription, classesNamespace);
+                var generatedCodeItem = new GeneratedCodeItem(classDescription.ClassName, code);
+                generatedCodeList.Add(generatedCodeItem);
+            }
+
+            return generatedCodeList;
+        }
+
+        private TypeTable LoadPlugins(string pluginsDirectory)
+        {
+            var pluginsLoader = new PluginsLoader();
+            return pluginsLoader.LoadTypesFromPlugins(pluginsDirectory);
+        }
+    }
+}
---
+++ DtoGenerator/DtoGenerator/PluginsLoader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using TypePluginInterface;
+
+namespace DtoGenerator
+{
+    internal class PluginsLoader
+    {
+        public TypeTable LoadTypesFromPlugins(string pluginsDirectoryPath)
+        {
+            var typeTable = new TypeTable();
+
+            foreach (var file in Directory.EnumerateFiles(Path.GetFullPath(pluginsDirectoryPath)))
+            {
+                try
+                {
+                    var assembly = Assembly.LoadFile(Path.GetFullPath(file));
+                    var exportedTypes = assembly.GetExportedTypes();
+
+                    var types = Array.FindAll(exportedTypes,
+                        type => typeof(INetType).IsAssignableFrom(type) && !type.IsAbstract);
+                    CreateInstances(typeTable, types);
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+
+            return typeTable;
+        }
+
+        private void CreateInstances(TypeTable typeTable, Type[] types)
+        {
+            foreach (var type in types)
+            {
+                var pluginInstance = (INetType)Activator.CreateInstance(type);
+                typeTable.AddType(pluginInstance);
+            }
+        }
+    }
+}
