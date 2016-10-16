1dd69730cf5d5a31a335ef003b1a231e276976ec
OksanaPiskunova <piskunova.oksana2014@yandex.ru>
2016-10-16 15:06:30 +0300

    Added class to perfomance result of code generation

---
+++ DtoGenerator/DtoGenerator/GeneratedCodeItem.cs
@@ -0,0 +1,14 @@
+﻿namespace DtoGenerator
+{
+    public sealed class GeneratedCodeItem
+    {
+        public string ClassName { get; private set; }
+        public string Code { get; private set; }
+
+        public GeneratedCodeItem(string className, string code)
+        {
+            ClassName = className;
+            Code = code;
+        }
+    }
+}
